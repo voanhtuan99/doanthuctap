8989f38749430e785f8e8529ac77ffe85cac63bc
Tuan Vo <tuanvo19991604@gmail.com>
2021-08-26 21:20:19 +0700

    deploy

--- src/customer/login.js
+++ src/customer/login.js
@@ -2,16 +2,31 @@ import axios from "axios";
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import BackHomepage from "./backHomepage";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ClipLoader from "react-spinners/ClipLoader";
+
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
             email: '',
             password: '',
+            isShowQuenMK: false,
+            isShowMaXN: false,
+            isShowMKMoi: false,
+            loading: false
         }
         this.handleChangeEmail = this.handleChangeEmail.bind(this)
         this.handleChangePassword = this.handleChangePassword.bind(this)
         this.btnClickLogin = this.btnClickLogin.bind(this)
+        this.handleQuenMK = this.handleQuenMK.bind(this)
+        this.closeQuenMK = this.closeQuenMK.bind(this)
+        this.handleMaXN = this.handleMaXN.bind(this)
+        this.closeMaXN = this.closeMaXN.bind(this)
+        this.handleMKMoi = this.handleMKMoi.bind(this)
+        this.closeMKMoi = this.closeMKMoi.bind(this)
+        this.doimkthanhcong = this.doimkthanhcong.bind(this)
     }
 
 
@@ -19,8 +34,41 @@ class Login extends Component {
 
     }
 
+    handleQuenMK() {
+        this.setState({
+            isShowQuenMK: true
+        })
+    }
+    closeQuenMK() {
+        this.setState({
+            isShowQuenMK: false
+        })
+    }
+    handleMaXN() {
+        this.setState({
+            isShowMaXN: true
+        })
+    }
+    closeMaXN() {
+        this.setState({
+            isShowMaXN: false
+        })
+    }
+    handleMKMoi() {
+        this.setState({
+            isShowMKMoi: true
+        })
+    }
+    closeMKMoi() {
+        this.setState({
+            isShowMKMoi: false
+        })
+    }
 
     btnClickLogin() {
+        this.setState({
+            loading: true
+        })
         if (this.state.email === "") {
             document.querySelector(".gmailerr").innerHTML = "<p>Vui lòng điền gmail</p>"
         }
@@ -53,8 +101,14 @@ class Login extends Component {
                     localStorage.setItem("role", response.data.user.role)
                     this.props.history.push("/admin")
                 }
+                this.setState({
+                    loading: false
+                })
 
             }).catch(err => {
+                this.setState({
+                    loading: false
+                })
                 document.querySelector(".passerr").innerHTML = "<p>Gmail hoặc mật khẩu không chính xác</p>"
             })
         }
@@ -68,10 +122,43 @@ class Login extends Component {
         this.setState({ email: event.target.value });
     }
 
+    doimkthanhcong() {
+        this.setState({
+            isShowMKMoi: false,
+            isShowMaXN: false,
+            isShowQuenMK: false
+        })
+        toast.success('Đổi mật khẩu thành công!', {
+            position: "top-right",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+
     render() {
-        let { email, password } = this.state
+        let { isShowMKMoi, isShowMaXN, isShowQuenMK, loading } = this.state
+        let formquenmk, formmaxn, formmkmoi
+        if (isShowQuenMK === true) {
+            formquenmk = <FormQuenMatKhau closeQuenMK={this.closeQuenMK} handleMaXN={this.handleMaXN} />
+        }
+        if (isShowMaXN === true) {
+            formmaxn = <FormMaXNQuenMK closeMaXN={this.closeMaXN} handleMKMoi={this.handleMKMoi} />
+        }
+        if (isShowMKMoi === true) {
+            formmkmoi = <FormNhapMKMoi closeMKMoi={this.closeMKMoi} doimkthanhcong={this.doimkthanhcong} />
+        }
         return (<div>
             <BackHomepage />
+            <ToastContainer />
+            {/* <FormNhapMKMoi /> */}
+            {/* <FormMaXNQuenMK /> */}
+            {formquenmk}
+            {formmaxn}
+            {formmkmoi}
             <div className="dangnhap" style={{ backgroundImage: 'url(https://th.bing.com/th/id/R.17bacde7417ced8892d5257d7eae082e?rik=G1bJcb6dCfA3gA&riu=http%3a%2f%2ffile.vforum.vn%2fhinh%2f2016%2f08%2fhinh-anh-quyen-sach-dep-9.jpg&ehk=LNiVzdZFW2yx%2blD8tTamGKilqgD1%2bdQdcRVX2vrxyaA%3d&risl=&pid=ImgRaw)' }}>
                 <div className="formdangnhap">
                     <div className="form__title">
@@ -98,7 +185,10 @@ class Login extends Component {
                     </div>
                     <div className="err passerr"><br></br></div>
 
-                    <h5>Quên mật khẩu</h5>
+                    <h5 onClick={this.handleQuenMK}>Quên mật khẩu</h5>
+                    <div className="loaddoimk">
+                        {loading ? (<ClipLoader size={20} color={"#F37A24"} loading={loading} />) :
+                            <br></br>}</div>
                     <button className="btn-green" onClick={this.btnClickLogin}>Đăng nhập</button>
                     <div className="form__regis">
                         <div className="contain">
@@ -111,6 +201,243 @@ class Login extends Component {
 
                 </div>
             </div></div>)
+    }
+}
+
+class FormQuenMatKhau extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: '',
+            loading: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.checkgmail = this.checkgmail.bind(this)
+        this.closeQuenMK = this.closeQuenMK.bind(this)
+    }
+
+    checkgmail() {
+        this.setState({
+            loading: true
+        })
+        axios({
+            method: "POST",
+            url: `https://tttn.herokuapp.com/api/auth/kiemtragmail`,
+            data: {
+                email: this.state.input
+            }
+        })
+            .then(response => {
+                document.querySelector(".saimxn").innerHTML = "<p>Email không tồn tại</p>"
+                this.setState({
+                    loading: false
+                })
+            })
+            .catch((err) => {
+                this.props.closeQuenMK()
+                this.props.handleMaXN()
+                axios({
+                    method: "POST",
+                    url: `https://tttn.herokuapp.com/api/select/otpUser`,
+                    data: {
+                        email: this.state.input
+
+                    }
+                })
+                    .then(response => {
+                        sessionStorage.setItem('maxnquenmk', response.data.otp)
+                        sessionStorage.setItem('email', this.state.input)
+                        this.setState({
+                            loading: false
+                        })
+                    })
+                    .catch(err => {
+
+                    })
+            })
+    }
+    closeQuenMK() {
+        this.props.closeQuenMK()
+        sessionStorage.removeItem('maxnquenmk')
+        sessionStorage.removeItem('email')
+    }
+    handleChange(e) {
+        this.setState({
+            input: e.target.value
+        })
+    }
+    render() {
+        let { loading } = this.state
+        return (
+            <div className="addProductoverlay">
+                <div className="formxacnhantk">
+                    <h3>Quên mật khẩu</h3>
+                    <input type="text" placeholder="Mời nhập gmail" onChange={this.handleChange} />
+                    <div className="err saimxn"><br /></div>
+                    <div className="loaddoimk">
+                        {loading ? (<ClipLoader size={20} color={"#F37A24"} loading={loading} />) :
+                            <br></br>}
+                    </div>
+                    <div className="groupbtn">
+                        <button onClick={this.checkgmail}>Xác nhận</button>
+                        <button onClick={this.closeQuenMK}>Thoát</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class FormMaXNQuenMK extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.closeMaXN = this.closeMaXN.bind(this)
+        this.handleMKMoi = this.handleMKMoi.bind(this)
+    }
+
+
+    closeMaXN() {
+        this.props.closeMaXN()
+        sessionStorage.removeItem('maxnquenmk')
+        sessionStorage.removeItem('email')
+    }
+    handleMKMoi() {
+        if (sessionStorage.getItem("maxnquenmk") === this.state.input) {
+            this.props.handleMKMoi()
+        }
+        else document.querySelector(".saimxnmk").innerHTML = "<p>Sai mã xác nhận</p>"
+
+    }
+    handleChange(e) {
+        this.setState({
+            input: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div className="addProductoverlay">
+                <div className="formxacnhantk">
+                    <h3>Nhập mã xác nhận</h3>
+                    <input type="text" placeholder="Mời nhập mã xác nhận" onChange={this.handleChange} />
+                    <div className="err saimxnmk"><br /></div>
+                    <div className="groupbtn">
+                        <button onClick={this.handleMKMoi}>Xác nhận</button>
+                        <button onClick={this.closeMaXN}>Thoát</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class FormNhapMKMoi extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mk1: '',
+            mk2: '',
+            user: {},
+        }
+        this.handleChangeMK1 = this.handleChangeMK1.bind(this)
+        this.handleChangeMK2 = this.handleChangeMK2.bind(this)
+        this.doimkthanhcong = this.doimkthanhcong.bind(this)
+        this.closeMKMoi = this.closeMKMoi.bind(this)
+    }
+    handleChangeMK1(e) {
+        this.setState({
+            mk1: e.target.value
+        })
+        if (e.target.value !== '') {
+            document.querySelector('.saimxn1').innerHTML = '<br></br>'
+        }
+    }
+    handleChangeMK2(e) {
+        this.setState({
+            mk2: e.target.value
+        })
+        if (e.target.value !== '') {
+            document.querySelector('.saimxn2').innerHTML = '<br></br>'
+        }
+    }
+    componentDidMount() {
+        axios({
+            method: "POST",
+            url: `https://tttn.herokuapp.com/api/auth/selectgmaildoimk`,
+            data: {
+                email: sessionStorage.getItem('email')
+            }
+        })
+            .then(response => {
+                this.setState({
+                    user: response.data.userselect
+                })
+            })
+    }
+    doimkthanhcong() {
+        if (this.state.mk1 === '') {
+            document.querySelector('.saimxn1').innerHTML = '<p>Không được để trống mật khẩu</p>'
+        }
+        if (this.state.mk2 === '') {
+            document.querySelector('.saimxn2').innerHTML = '<p>Không được để trống xác nhận mật khẩu</p>'
+        }
+        if (this.state.mk2 !== this.state.mk1) {
+            document.querySelector('.saimxn2').innerHTML = '<p>Mật khẩu và xác nhận mật khẩu phải giống nhau</p>'
+        }
+        if (this.state.mk1 !== '' && this.state.mk2 !== '' && this.state.mk2 === this.state.mk1) {
+            axios({
+                method: "PUT",
+                url: `https://tttn.herokuapp.com/api/auth/doimk`,
+                data: {
+                    email: this.state.user.email,
+                    name: this.state.user.name,
+                    sdt: this.state.user.sdt,
+                    diachi: this.state.user.diachi,
+                    role: this.state.user.role,
+                    password: this.state.mk1
+                }
+            })
+                .then(response => {
+                    this.props.doimkthanhcong()
+                    sessionStorage.removeItem('maxnquenmk')
+                    sessionStorage.removeItem('email')
+                })
+                .catch(err => {
+                    document.querySelector('.saimxn2').innerHTML = '<p>Đổi mk thất bại</p>'
+                    sessionStorage.removeItem('maxnquenmk')
+                    sessionStorage.removeItem('email')
+                })
+        }
+        // this.props.doimkthanhcong()
+    }
+
+    closeMKMoi() {
+        this.props.closeMKMoi()
+        sessionStorage.removeItem('maxnquenmk')
+        sessionStorage.removeItem('email')
+    }
+
+    render() {
+        console.log(this.state.user)
+        return (
+            <div className="addProductoverlay">
+                <div className="formnhapmkmoi">
+                    <h3>Nhập mật khẩu mới</h3>
+                    <p>Gmail: {sessionStorage.getItem('email')}</p>
+                    <input type="password" placeholder="Mời nhập mật khẩu mới" onChange={this.handleChangeMK1} />
+                    <div className="err saimxn1"><br /></div>
+                    <input type="password" placeholder="Xác nhận mật khẩu" onChange={this.handleChangeMK2} />
+                    <div className="err saimxn2"><br /></div>
+                    <div className="groupbtn">
+                        <button onClick={this.doimkthanhcong}>Xác nhận</button>
+                        <button onClick={this.closeMKMoi}>Thoát</button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
--- src/customer/register.js
+++ src/customer/register.js
@@ -78,7 +78,7 @@ class RegistPage extends Component {
 
         axios({
             method: "POST",
-            url: `http://localhost:5000/api/auth/kiemtragmail`,
+            url: `https://tttn.herokuapp.com/api/auth/kiemtragmail`,
             data: {
                 email: this.state.email
             }
@@ -136,7 +136,7 @@ class RegistPage extends Component {
             })
             axios({
                 method: "POST",
-                url: `http://localhost:5000/api/select/otpUser`,
+                url: `https://tttn.herokuapp.com/api/select/otpUser`,
                 data: {
                     email: this.state.email
                 }
@@ -226,10 +226,7 @@ class RegistPage extends Component {
                     <div className="form__input">
                         <input type="text" placeholder="Số điện thoại" onChange={this.handleSdt} value={this.state.sdt} />
                         <div className="form__icon">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" className="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                <path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z">
-                                </path>
-                            </svg>
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" class="svg-inline--fa fa-phone fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg>
                         </div>
                     </div>
                     <div className="err sdterr"><br></br></div>
@@ -237,10 +234,7 @@ class RegistPage extends Component {
                     <div className="form__input">
                         <input type="text" placeholder="Địa chỉ" onChange={this.handleDiachi} value={this.state.diachi} />
                         <div className="form__icon">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" className="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                <path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z">
-                                </path>
-                            </svg>
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map" class="svg-inline--fa fa-map fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M0 117.66v346.32c0 11.32 11.43 19.06 21.94 14.86L160 416V32L20.12 87.95A32.006 32.006 0 0 0 0 117.66zM192 416l192 64V96L192 32v384zM554.06 33.16L416 96v384l139.88-55.95A31.996 31.996 0 0 0 576 394.34V48.02c0-11.32-11.43-19.06-21.94-14.86z"></path></svg>
                         </div>
                     </div>
                     <div className="err diachierr"><br></br></div>
@@ -281,7 +275,7 @@ class FormMaXN extends Component {
             console.log(this.props.user)
             axios({
                 method: "POST",
-                url: `http://localhost:5000/api/auth/register`,
+                url: `https://tttn.herokuapp.com/api/auth/register`,
                 data: this.props.user
             })
                 .then(response => {
